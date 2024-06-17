f11024d50acc8e73b116b18cd179d908fd60754f
tinokim <65646900+tinokim@users.noreply.github.com>
2024-06-18 00:16:47 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,20 +1,8 @@
-# 과목의 수를 입력
 n = int(input())
-
-# 성적을 입력받아 리스트로 저장
-scores = list(map(int, input().split()))
-
-# 최댓값 M을 찾음
-M = max(scores)
-
-# 모든 점수를 점수/M*100으로 조작
-scores_2 = []  # 저장할 빈 리스트 생성
-for score in scores:
-    f_s = (score / M) * 100  # 점수를 조작
-    scores_2.append(f_s)  # 조작된 점수를 리스트에 추가
-
-# 조작된 점수의 평균을 계산
-real_avg = sum(scores_2) / n
-
-# 새로운 평균 출력
-print(real_avg)
+jum = list(map(int, input().split()))
+maxJum = max(jum)
+avg = 0.0
+for i in jum:
+    avg = avg + ((i / maxJum) * 100)
+    
+print(avg / n)
