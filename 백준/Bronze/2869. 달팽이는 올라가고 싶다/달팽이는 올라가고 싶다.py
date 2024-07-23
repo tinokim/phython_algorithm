a58cbf1fa6cf12a40c1576049c58ef1c976d6bfa
tinokim <65646900+tinokim@users.noreply.github.com>
2024-07-23 23:39:47 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -0,0 +1,9 @@
+A, B, V = map(int, input().split()); # 낮 올 // 밤 내 // 총 길이
+
+if V < A : 
+    print(1);
+else:
+    if (V-A) % (A-B) == 0 :
+        print((V-A) // (A-B) +1)
+    else :
+        print((V-A) // (A-B) +2)
