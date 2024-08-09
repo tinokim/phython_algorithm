bcf98e7d19a6cf1789123b663e32501bc870117c
tinokim <65646900+tinokim@users.noreply.github.com>
2024-08-09 15:22:41 +0900

    [level 2] Title: 올바른 괄호, Time: 6.16 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    cnt = 0
+    for i in s:
+        if i == '(':
+            cnt += 1
+            continue
+            
+        cnt -= 1
+        if cnt < 0:
+            return False
+        
+    if cnt != 0:
+        return False
+    return True
